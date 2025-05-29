116306ac984ea640e33e10991ebb50ccca68ae99
korololes <lesyakoroleva2121@gmail.com>
2025-05-30 00:25:25 +0300

    Реализовано: загрузка данных из файла, отображение этих данных в таблице на форме

--- TempForm.Designer.cs
+++ TempForm.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(12, 741);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(142, 71);
+            this.btnLoadData.TabIndex = 0;
+            this.btnLoadData.Text = "Загрузить файл";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(565, 722);
+            this.dataGridView.TabIndex = 4;
+            // 
+            // TempForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1518, 824);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnLoadData);
+            this.Name = "TempForm";
             this.Text = "Данные о температуре";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.DataGridView dataGridView;
     }
 }
--- TempForm.cs
+++ TempForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace WindowsFormsApp1
 {
     public partial class TempForm : Form
     {
+        private DataTable weatherData;
+  
         public TempForm()
         {
             InitializeComponent();
         }
+
+        // Кнопка "Загрузить файл"
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Excel Files|*.xlsx;*.xls",
+                Title = "Выберите файл с данными о погоде"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    LoadExcelData(openFileDialog.FileName);
+                    DisplayData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при загрузке файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Обработка данных из файла
+        private void LoadExcelData(string filePath)
+        {
+            var excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
+            Excel.Worksheet worksheet = workbook.Sheets[1];
+            Excel.Range range = worksheet.UsedRange;
+
+            weatherData = new DataTable();
+
+            // Добавляем колонки
+            for (int i = 1; i <= range.Columns.Count; i++)
+            {
+                string columnName = (range.Cells[1, i] as Excel.Range).Value2?.ToString() ?? $"Column{i}";
+                weatherData.Columns.Add(columnName);
+            }
+
+            // Заполняем данные
+            for (int i = 2; i <= range.Rows.Count; i++)
+            {
+                DataRow row = weatherData.NewRow();
+                for (int j = 1; j <= range.Columns.Count; j++)
+                {
+                    var cell = range.Cells[i, j] as Excel.Range;
+                    object value = cell.Value2;
+
+                    if (value != null)
+                    {
+                        // Обработка дат
+                        if (j == 1 && value is double)
+                        {
+                            DateTime date = DateTime.FromOADate((double)value);
+                            row[j - 1] = date.ToString("dd.MM.yyyy");
+                        }
+                        else
+                        {
+                            // Унификация числового формата
+                            string strValue = value.ToString();
+                            if (double.TryParse(strValue.Replace(",", "."), System.Globalization.NumberStyles.Any,
+                                System.Globalization.CultureInfo.InvariantCulture, out double numValue))
+                            {
+                                row[j - 1] = numValue;
+                            }
+                            else
+                            {
+                                row[j - 1] = strValue;
+                            }
+                        }
+                    }
+                }
+                weatherData.Rows.Add(row);
+            }
+
+            workbook.Close(false);
+            excelApp.Quit();
+        }
+
+        // Отображение таблицы
+        private void DisplayData()
+        {
+            dataGridView.DataSource = weatherData;
+            dataGridView.AutoResizeColumns();
+        }
+
+        
     }
 }
+
