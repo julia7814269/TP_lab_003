3be50024805e2e5ebce81504a9104ce559fb6933
julia7814269 <154632642+julia7814269@users.noreply.github.com>
2025-05-30 14:15:54 +0300

    Разработана кнопка открытия и вывода на экран таблицы данных

--- RoadForm.Designer.cs
+++ RoadForm.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(-10, 1118);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 82;
+            this.dataGridView.RowTemplate.Height = 33;
+            this.dataGridView.Size = new System.Drawing.Size(2647, 340);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(2, 67);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(2142, 1045);
+            this.chart.TabIndex = 1;
+            this.chart.Text = "chart1";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(2226, 67);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(357, 46);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "Открыть таблицу из файла";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // RoadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2711, 1500);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "RoadForm";
             this.Text = "Данные о дорогах";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Button btnOpenFile;
     }
 }
--- RoadForm.cs
+++ RoadForm.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WindowsFormsApp1
 {
     public partial class RoadForm : Form
     {
+        private DataTable dataTable;
         public RoadForm()
         {
             InitializeComponent();
+            dataTable = new DataTable();
+            dataGridView.DataSource = dataTable;
+        }
+        // Кнопка открытия файла
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Excel Files|*.xlsx;*.xls",
+                Title = "Select Road Data File"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    LoadExcelData(openFileDialog.FileName);
+                    DisplayData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        // Загрузка данных из таблицы EXCEL
+        private void LoadExcelData(string filePath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
+            Excel.Worksheet worksheet = workbook.Sheets[1];
+            Excel.Range range = worksheet.UsedRange;
+
+            dataTable.Clear();
+            dataTable.Columns.Clear();
+
+            // Добавление колонок
+            for (int c = 1; c <= range.Columns.Count; c++)
+            {
+                string columnName = (range.Cells[1, c] as Excel.Range)?.Value?.ToString() ?? $"Column{c}";
+                dataTable.Columns.Add(columnName);
+            }
+
+            // Добавление данных
+            for (int r = 2; r <= range.Rows.Count; r++)
+            {
+                DataRow dataRow = dataTable.NewRow();
+                for (int c = 1; c <= range.Columns.Count; c++)
+                {
+                    dataRow[c - 1] = (range.Cells[r, c] as Excel.Range)?.Value;
+                }
+                dataTable.Rows.Add(dataRow);
+            }
+
+            workbook.Close(false);
+            excelApp.Quit();
+        }
+        // Вывод таблицы
+        private void DisplayData()
+        {
+            dataGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
         }
     }
 }
